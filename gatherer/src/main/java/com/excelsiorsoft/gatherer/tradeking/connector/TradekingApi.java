8cb5d37789d7839f48bc207df2f5299febc45018
sleyzerzon <senya22@yahoo.com>
2015-05-14 14:05:59 -0400

    creating TK Api

---
+++ gatherer/src/main/java/com/excelsiorsoft/gatherer/tradeking/connector/TradekingApi.java
@@ -0,0 +1,40 @@
+package com.excelsiorsoft.gatherer.tradeking.connector;
+
+import java.io.Serializable;
+
+import org.scribe.builder.api.DefaultApi10a;
+import org.scribe.builder.api.DefaultApi20;
+import org.scribe.model.OAuthConfig;
+import org.scribe.model.Token;
+
+public class TradekingApi extends DefaultApi10a implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7378772448374411605L;
+
+	@Override
+	public String getRequestTokenEndpoint() {
+		
+		return "https://developers.tradeking.com/oauth/request_token";
+		
+	}
+
+	@Override
+	public String getAccessTokenEndpoint() {
+		
+		return "https://developers.tradeking.com/oauth/access_token";
+		
+	}
+
+	@Override
+	public String getAuthorizationUrl(Token requestToken) {
+		
+		return "https://developers.tradeking.com/oauth/authorize?oauth_token=%s";
+		
+	}
+
+	
+
+}
---
+++ gatherer/src/main/java/com/excelsiorsoft/gatherer/tradeking/connector/TradekingApiTest.java
@@ -0,0 +1,48 @@
+package com.excelsiorsoft.gatherer.tradeking.connector;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.junit.Test;
+
+
+public class TradekingApiTest {
+
+	@Test
+	public void validEndpointsTest()
+	{
+		TradekingApi api = new TradekingApi();
+		assertTrue("Request Token Endpoint is not valid",isValid(api.getRequestTokenEndpoint()));
+		assertTrue("Access Token Endpoint is not valid",isValid(api.getAccessTokenEndpoint()));
+	}
+
+	public boolean isValid(String urlString)
+	{
+		URL url = null;
+		try
+		{
+			url = new URL(urlString);
+		}
+		catch (MalformedURLException e)
+		{
+			return false;
+		}
+		URLConnection conn;
+		try
+		{
+			conn = url.openConnection();
+			conn.connect();
+		}
+		catch (IOException e)
+		{
+			return false;
+		}
+		return true;
+	}
+
+
+}
