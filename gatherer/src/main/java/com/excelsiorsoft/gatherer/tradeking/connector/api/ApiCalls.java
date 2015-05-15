c5411c782b87cd407d714511a5c5ac551c2b8297
sleyzerzon <senya22@yahoo.com>
2015-05-15 10:53:40 -0400

    cleaning up

--- gatherer/src/main/java/com/excelsiorsoft/gatherer/tradeking/connector/api/ApiCalls.java
+++ gatherer/src/main/java/com/excelsiorsoft/gatherer/tradeking/connector/api/ApiCalls.java
@@ -4,16 +4,12 @@ package com.excelsiorsoft.gatherer.tradeking.connector.api;
 import static freemarker.template.ObjectWrapper.BEANS_WRAPPER;
 import static org.scribe.model.Verb.GET;
 
-
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Map;
-
 import org.scribe.model.Verb;
-
 import freemarker.template.Configuration;
-
 import freemarker.template.Template;
 
 
@@ -33,14 +29,12 @@ public class ApiCalls {
 		String getTemplate(); 
 	}
 
-	public static String getMarketClock(String format) throws Throwable
-	{
+	public static String getMarketClock(String format) throws Throwable	{
 		return resolveString( MARKET.CLOCK, null,format);
 	}
 	
 	
-	public static String resolveString(CallType type,
-			Map<String, String> params, String format) throws Throwable {
+	public static String resolveString(CallType type, Map<String, String> params, String format) throws Throwable {
 
 		Configuration cfg = new Configuration();
 		cfg.setObjectWrapper(BEANS_WRAPPER);
@@ -54,24 +48,23 @@ public class ApiCalls {
 		return out.toString();
 	}
 	
-	public enum MARKET implements CallType
-	{
+	public enum MARKET implements CallType	{
 		CLOCK(GET, "https://api.tradeking.com/v1/market/clock"), 
-		EXT_QUOTES(GET,"https://api.tradeking.com/v1/market/ext/quotes"), 
-		STREAM_EXT_QUOTES(GET,"https://stream.tradeking.com/v1/market/quotes"), 
-		NEWS_SEARCH(GET,"https://api.tradeking.com/v1/market/news/search"), 
-		NEWS_ID(GET,"https://api.tradeking.com/v1/market/news/"), 
-		OPTIONS_SEARCH(GET,"https://api.tradeking.com/v1/market/options/search"), 
-		OPTIONS_STRIKES(GET,"https://api.tradeking.com/v1/market/options/strikes"), 
-		OPTIONS_EXPIRATIONS(GET,"https://api.tradeking.com/v1/market/options/expirations"), 
-		TIMESALES(GET,"https://api.tradeking.com/v1/market/timesales"), 
-		TOPLISTS_VOLUME(GET,"https://api.tradeking.com/v1/market/toplists/topvolume"), 
-		TOPLISTS_LOSERS_DOLLAR(GET,"https://api.tradeking.com/v1/market/toplists/toplosers"), 
-		TOPLISTS_LOSERS_PERCENTAGE(GET,"https://api.tradeking.com/v1/market/toplists/toppctlosers"), 
-		TOPLISTS_ACTIVE(GET,"https://api.tradeking.com/v1/market/toplists/topactive"), 
-		TOPLISTS_GAINERS_DOLLAR_AMT(GET,"https://api.tradeking.com/v1/market/toplists/topgainers"), 
-		TOPLISTS_GAINERS_PERCENTAGE(GET,"https://api.tradeking.com/v1/market/toplists/toppctgainers"), 
-		TOPLISTS_GAINERS_ACTIVE_DOLLAR_AMT(GET,"https://api.tradeking.com/v1/market/toplists/topactivegainersbydollarvalue");
+		EXT_QUOTES(GET, "https://api.tradeking.com/v1/market/ext/quotes"), 
+		STREAM_EXT_QUOTES(GET, "https://stream.tradeking.com/v1/market/quotes"), 
+		NEWS_SEARCH(GET, "https://api.tradeking.com/v1/market/news/search"), 
+		NEWS_ID(GET, "https://api.tradeking.com/v1/market/news/"), 
+		OPTIONS_SEARCH(GET, "https://api.tradeking.com/v1/market/options/search"), 
+		OPTIONS_STRIKES(GET, "https://api.tradeking.com/v1/market/options/strikes"), 
+		OPTIONS_EXPIRATIONS(GET, "https://api.tradeking.com/v1/market/options/expirations"), 
+		TIMESALES(GET, "https://api.tradeking.com/v1/market/timesales"), 
+		TOPLISTS_VOLUME(GET, "https://api.tradeking.com/v1/market/toplists/topvolume"), 
+		TOPLISTS_LOSERS_DOLLAR(GET, "https://api.tradeking.com/v1/market/toplists/toplosers"), 
+		TOPLISTS_LOSERS_PERCENTAGE(GET, "https://api.tradeking.com/v1/market/toplists/toppctlosers"), 
+		TOPLISTS_ACTIVE(GET, "https://api.tradeking.com/v1/market/toplists/topactive"), 
+		TOPLISTS_GAINERS_DOLLAR_AMT(GET, "https://api.tradeking.com/v1/market/toplists/topgainers"), 
+		TOPLISTS_GAINERS_PERCENTAGE(GET, "https://api.tradeking.com/v1/market/toplists/toppctgainers"), 
+		TOPLISTS_GAINERS_ACTIVE_DOLLAR_AMT(GET, "https://api.tradeking.com/v1/market/toplists/topactivegainersbydollarvalue");
 
 		private Verb httpMethod;
 		private String template;
