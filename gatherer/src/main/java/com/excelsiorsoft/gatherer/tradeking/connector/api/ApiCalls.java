a21e473f325c054deca3f18775fc39b2d77ab9b5
sleyzerzon <senya22@yahoo.com>
2015-05-15 08:55:11 -0400

    gradual replacement of request generation logic

---
+++ gatherer/src/main/java/com/excelsiorsoft/gatherer/tradeking/connector/api/ApiCalls.java
@@ -0,0 +1,79 @@
+package com.excelsiorsoft.gatherer.tradeking.connector.api;
+
+import static org.scribe.model.Verb.GET;
+
+import java.util.Map;
+
+import org.scribe.model.Verb;
+
+import com.excelsiorsoft.gatherer.tradeking.connector.api.ApiCall.MARKET;
+
+/**
+ * A builder for different sorts of TradeKing API calls - replacement for {@link ApiCall}
+ * 
+ * @author sleyzerzon
+ *
+ */
+public class ApiCalls {
+	
+
+	public interface CallType {}
+
+	public static String getMarketClock(String format)
+	{
+		return resolveString( MARKET.CLOCK, null,format);
+	}
+	
+	
+	public static String resolveString(CallType type, Map<String, String> params, String format){
+	
+		return null;
+	}
+	
+	public enum MARKET implements CallType
+	{
+		CLOCK(GET, "https://api.tradeking.com/v1/market/clock"), 
+		EXT_QUOTES(GET,"https://api.tradeking.com/v1/market/ext/quotes"), 
+		STREAM_EXT_QUOTES(GET,"https://stream.tradeking.com/v1/market/quotes"), 
+		NEWS_SEARCH(GET,"https://api.tradeking.com/v1/market/news/search"), 
+		NEWS_ID(GET,"https://api.tradeking.com/v1/market/news/"), 
+		OPTIONS_SEARCH(GET,"https://api.tradeking.com/v1/market/options/search"), 
+		OPTIONS_STRIKES(GET,"https://api.tradeking.com/v1/market/options/strikes"), 
+		OPTIONS_EXPIRATIONS(GET,"https://api.tradeking.com/v1/market/options/expirations"), 
+		TIMESALES(GET,"https://api.tradeking.com/v1/market/timesales"), 
+		TOPLISTS_VOLUME(GET,"https://api.tradeking.com/v1/market/toplists/topvolume"), 
+		TOPLISTS_LOSERS_DOLLAR(GET,"https://api.tradeking.com/v1/market/toplists/toplosers"), 
+		TOPLISTS_LOSERS_PERCENTAGE(GET,"https://api.tradeking.com/v1/market/toplists/toppctlosers"), 
+		TOPLISTS_ACTIVE(GET,"https://api.tradeking.com/v1/market/toplists/topactive"), 
+		TOPLISTS_GAINERS_DOLLAR_AMT(GET,"https://api.tradeking.com/v1/market/toplists/topgainers"), 
+		TOPLISTS_GAINERS_PERCENTAGE(GET,"https://api.tradeking.com/v1/market/toplists/toppctgainers"), 
+		TOPLISTS_GAINERS_ACTIVE_DOLLAR_AMT(GET,"https://api.tradeking.com/v1/market/toplists/topactivegainersbydollarvalue");
+
+		private Verb httpMethod;
+		private String template;
+
+		MARKET(Verb httpMethod, String template)
+		{
+			this.httpMethod = httpMethod;
+			this.template = template;
+		
+		}
+		
+		public Verb getHttpMethod() {
+			return httpMethod;
+		}
+
+		public String getTemplate() {
+			return template;
+		}
+
+
+		
+
+		public Verb getVerb()
+		{
+			return Verb.GET;
+		}
+	}	
+
+}
