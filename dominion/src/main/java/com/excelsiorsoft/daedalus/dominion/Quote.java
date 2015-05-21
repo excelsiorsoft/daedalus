3c74443456e17372e55352e740729aa319180681
sleyzerzon <senya22@yahoo.com>
2015-05-21 16:24:26 -0400

    adding more classes

--- dominion/src/main/java/com/excelsiorsoft/daedalus/dominion/Quote.java
+++ dominion/src/main/java/com/excelsiorsoft/daedalus/dominion/Quote.java
@@ -1,5 +1,14 @@
 package com.excelsiorsoft.daedalus.dominion;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+
 public class Quote {
 
+	private Underlying underlying;
+
+	private final Map<String /*name TODO: or id?*/, ExpirationCycle /*respective cycle*/ > expirationCycles = new LinkedHashMap<>();
+	
 }
