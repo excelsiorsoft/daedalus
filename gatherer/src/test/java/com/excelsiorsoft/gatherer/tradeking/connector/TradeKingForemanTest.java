9c96da50c2e9db99a090c0142c0f17d1c367e014
sleyzerzon <senya22@yahoo.com>
2015-05-23 23:52:17 -0400

    polishing test a bit

--- gatherer/src/test/java/com/excelsiorsoft/gatherer/tradeking/connector/TradeKingForemanTest.java
+++ gatherer/src/test/java/com/excelsiorsoft/gatherer/tradeking/connector/TradeKingForemanTest.java
@@ -19,15 +19,20 @@ public class TradeKingForemanTest {
 	@Test
 	public void connectionTest()
 	{
-		
+		System.out.println("testing connection via TK OAuth...");
+		System.out.println("==============================");
 		TradeKingForeman foreman = new TradeKingForeman();
 		assertTrue("Foreman does not have OAuth Service", !foreman.isConnected());
+		System.out.println("connected fine.");
+		System.out.println("==============================");
 		
 	}
 	
 	@Test
 	public void marketClockApiCall() throws Throwable {
 		
+		System.out.println("market/clock call...");
+		System.out.println("==============================");
 		TradeKingForeman foreman = new TradeKingForeman();
 		
 
@@ -38,20 +43,26 @@ public class TradeKingForemanTest {
 		
 		XmlHandler handler = new XmlHandler();
 		handler.parseMarketClock(response);
+		System.out.println("==============================");
 	}	
 	
 	@Test
 	public void marketExtQuotesApiCall() throws Throwable {
 		
+		System.out.println("market/ext/quotes call...");
+		System.out.println("==============================");
 		TradeKingForeman foreman = new TradeKingForeman();
 		System.out.println(foreman.makeAPICall(getExtQuotes(xml, "slw, slw160115P00020000, slw160115P00021000 ", "")).toString());
 		//System.out.println(foreman.makeAPICall(getExtQuotes(XML, "gasl, gasl150918C00004000 ", "")).toString());
+		System.out.println("==============================");
 		
 	}
 	
 	@Test
 	public void exploringResponse() throws Throwable {
 		
+		System.out.println("exploring TK API response ...");
+		System.out.println("==============================");
 		TradeKingForeman foreman = new TradeKingForeman();
 		System.out.println("Calls remaining: " + foreman.makeAPICall(getExtQuotes(xml, "slw, slw160115P00020000, slw160115P00021000 ", "")).getCallsRemaining());
 		System.out.println("Calls used: " + foreman.makeAPICall(getExtQuotes(xml, "slw, slw160115P00020000, slw160115P00021000 ", "")).getCallsUsed());
@@ -64,22 +75,29 @@ public class TradeKingForemanTest {
 		System.out.println("Calls used: " + foreman.makeAPICall(getExtQuotes(xml, "slw, slw160115P00020000, slw160115P00021000 ", "")).getCallsUsed());
 		System.out.println("Rate limit expiration: " + foreman.makeAPICall(getExtQuotes(xml, "slw, slw160115P00020000, slw160115P00021000 ", "")).getRateLimitExpiration());
 		System.out.println("Total calls allowed: " + foreman.makeAPICall(getExtQuotes(xml, "slw, slw160115P00020000, slw160115P00021000 ", "")).getTotalCallsAllowed());
+		System.out.println("==============================");
 	}	
 	
 	
 	@Test
 	public void marketOptionsExpirationsApiCall() throws Throwable {
 		
+		System.out.println("market/options/expirations call...");
+		System.out.println("==============================");
 		TradeKingForeman foreman = new TradeKingForeman();
 		System.out.println(foreman.makeAPICall(getOptionsExpirations(xml, "slw ")).toString());
+		System.out.println("==============================");
 		
 	}
 	
 	@Test
 	public void marketOptionsStrikesApiCall() throws Throwable {
 		
+		System.out.println("market/options/strikes call...");
+		System.out.println("==============================");
 		TradeKingForeman foreman = new TradeKingForeman();
 		System.out.println(foreman.makeAPICall(getOptionsStrikes(xml, "slw ")).toString());
+		System.out.println("==============================");
 		
 	}
 
