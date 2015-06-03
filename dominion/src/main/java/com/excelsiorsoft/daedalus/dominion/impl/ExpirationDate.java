1b848c62904daae71ded3b3892069531214b66ed
sleyzerzon <senya22@yahoo.com>
2015-06-03 16:34:06 -0400

    singular expiration date

---
+++ dominion/src/main/java/com/excelsiorsoft/daedalus/dominion/impl/ExpirationDate.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.excelsiorsoft.daedalus.dominion.impl;
+
+
+
+/**
+ * 
+ * 
+ * @author sleyzerzon
+ *
+ */
+public class ExpirationDate {
+	
+	
+	private long timestamp;
+	
+	private String symbol;
+	
+	private String cycle;
+	
+	private ExpirationDate() {}
+	
+	
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ExpirationDate[timestamp=")
+				.append(timestamp)
+				.append(", symbol=")
+				.append(symbol)
+				.append(", cycle=")
+				.append(cycle)
+				
+				.append("]");
+		return builder.toString();
+	}
+	
+	public final static class ExpirationDateBuilder {
+		
+		private final ExpirationDate expirationDate = new ExpirationDate();
+		
+		private ExpirationDateBuilder(){};
+		
+		public static ExpirationDateBuilder builder() {
+			
+			ExpirationDateBuilder result = new ExpirationDateBuilder();
+			return result;
+		}
+		
+		public ExpirationDate build(){
+			
+			return expirationDate;
+		}
+		
+		public ExpirationDateBuilder forSymbol(String symbol){
+			expirationDate.symbol = symbol;
+			return this;
+		}
+		
+		public ExpirationDateBuilder forCycle(String date){
+			expirationDate.cycle = date;
+			return this;
+		}
+		
+}
+
+}
---
+++ dominion/src/test/java/com/excelsiorsoft/daedalus/dominion/impl/ExpirationDateTest.java
@@ -0,0 +1,39 @@
+package com.excelsiorsoft.daedalus.dominion.impl;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.excelsiorsoft.daedalus.dominion.impl.ExpirationDate.ExpirationDateBuilder;
+import com.excelsiorsoft.daedalus.dominion.impl.ExpirationDates.ExpirationDatesBuilder;
+
+public class ExpirationDateTest {
+
+private static ExpirationDateBuilder expDateBuilder;
+	
+	@BeforeClass
+	public static void setUp(){
+		
+		expDateBuilder = ExpirationDateBuilder.builder();
+	}
+	
+	
+	@Test
+	public void toStringTesting() {
+		
+		List<String> dates =  Arrays.asList(new String[]{"2015-06-05","2015-06-12","2015-06-19","2015-06-26","2015-07-02","2015-07-10","2015-07-17","2015-08-21","2015-09-18","2015-12-18","2016-01-15","2017-01-20"});
+		
+		ExpirationDate expirationDates = expDateBuilder
+
+				.forSymbol("SLW")
+				.forCycle("2015-06-05")
+				.build();
+		System.out.println(expirationDates);
+		
+	}
+
+}
