e2609f2d4e342cc17dff212c3c383e50c67e35c7
sleyzerzon <senya22@yahoo.com>
2015-06-08 21:15:40 -0400

    preparing to deal with option matrix

---
+++ dominion/src/main/java/com/excelsiorsoft/daedalus/dominion/impl/CacheableOptionMatrix.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.excelsiorsoft.daedalus.dominion.impl;
+
+import java.util.List;
+
+/**
+ * @author Simeon
+ *
+ */
+public class CacheableOptionMatrix {
+	
+	private String symbol;
+	
+	private List<String> expirationCycles;
+	
+	private List<String> strikesPerCycle;
+
+}
