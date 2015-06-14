ab6325198db8de6eacb3abaa189fc73bcef9c652
sleyzerzon <senya22@yahoo.com>
2015-06-14 00:14:42 -0400

    refreshing

--- dominion/src/test/java/com/excelsiorsoft/daedalus/dominion/impl/ExpirationDatesTest.java
+++ dominion/src/test/java/com/excelsiorsoft/daedalus/dominion/impl/ExpirationDatesTest.java
@@ -1,10 +1,13 @@
 package com.excelsiorsoft.daedalus.dominion.impl;
 
+import static java.util.Arrays.asList;
+
 import java.util.Arrays;
 import java.util.List;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 
 
 
@@ -27,7 +30,7 @@ public class ExpirationDatesTest {
 	@Test
 	public void toStringTesting() {
 		
-		List<String> dates =  Arrays.asList(new String[]{"2015-06-05","2015-06-12","2015-06-19","2015-06-26","2015-07-02","2015-07-10","2015-07-17","2015-08-21","2015-09-18","2015-12-18","2016-01-15","2017-01-20"});
+		List<String> dates =  asList(new String[]{"2015-06-05","2015-06-12","2015-06-19","2015-06-26","2015-07-02","2015-07-10","2015-07-17","2015-08-21","2015-09-18","2015-12-18","2016-01-15","2017-01-20"});
 		
 		ExpirationDates expirationDates = expDatesBuilder
 
