31fe744558ec6b5481367c31b09dbfd186c828cf
sleyzerzon <senya22@yahoo.com>
2015-09-10 16:54:52 -0400

    randomizer for more thorough testing

---
+++ gatherer/src/test/java/com/excelsiorsoft/gatherer/tradeking/market/flow/util/RandomPick.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.excelsiorsoft.gatherer.tradeking.market.flow.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author sleyzerzon
+ *
+ */
+public class RandomPick<T> {
+	
+	public final static <T> T randomFrom(List<T> elements){
+		
+		T result = null;
+		Random random = new Random();
+		int index = random.nextInt(elements.size());
+		result = elements.get(index);
+		return result;
+		
+	}
+	
+	public static void main(String [] args) {
+		
+		
+		for(int i = 0; i<20; i++) {
+			System.out.println(new RandomPick().randomFrom(Arrays.asList(new String[] {"Abraham Linkoln", "Jane Eire", "Richard Nixon", "Miss America"})));
+		}
+	}
+
+}
