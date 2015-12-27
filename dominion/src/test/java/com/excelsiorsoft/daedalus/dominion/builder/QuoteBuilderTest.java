413aa4eef4b864791498dfd5afdf16425c091126
sleyzerzon <senya22@yahoo.com>
2015-12-27 00:10:38 -0500

    do we need quote builder?

--- dominion/src/test/java/com/excelsiorsoft/daedalus/dominion/builder/QuoteBuilderTest.java
+++ dominion/src/test/java/com/excelsiorsoft/daedalus/dominion/builder/QuoteBuilderTest.java
@@ -17,6 +17,7 @@ public class QuoteBuilderTest {
 		String ticker = "SLW";
 		Quote quote = QuoteBuilder.withTicker(ticker).build();
 		assertNotNull("expect an instance of quote", quote);
+		System.out.println("result: "+quote);
 	}
 
 }
